dcf3705bfb187197fd63eabe646c1bf5c7c8c588
Terry <terrynyirenda123@gmail.com>
2025-06-06 10:45:32 -0400

    module 5 changes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,7 +19,11 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
+    path("postreview/<int:dealer_id>/", TemplateView.as_view(template_name="index.html")),
+    path("dealer/<int:dealer_id>/", TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers_page'),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
