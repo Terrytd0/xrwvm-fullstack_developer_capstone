757ec29b5df839215e7f15a6ade4d29dec6ca3fb
Terry Nyirenda <149245288+Terrytd0@users.noreply.github.com>
2025-06-06 12:52:44 +0200

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -14,7 +14,6 @@ const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'))
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
-
 const Reviews = require('./review');
 
 const Dealerships = require('./dealership');
